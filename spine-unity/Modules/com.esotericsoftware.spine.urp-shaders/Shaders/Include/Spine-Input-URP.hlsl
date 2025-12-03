5d3414f14393dfa20467937aee35457db48cf556
Brandon Reinhart <brandon@deadmoney.gg>
2025-12-03 13:15:25 -0800

    Add support for slot color lut and slot count and index.

--- spine-unity/Modules/com.esotericsoftware.spine.urp-shaders/Shaders/Include/Spine-Input-URP.hlsl
+++ spine-unity/Modules/com.esotericsoftware.spine.urp-shaders/Shaders/Include/Spine-Input-URP.hlsl
@@ -9,14 +9,20 @@
 #undef LIGHTMAP_ON
 
 CBUFFER_START(UnityPerMaterial)
-
-float4 _MainTex_ST;
-half _Cutoff;
-half4 _Color;
-half4 _Black;
-
+    float4 _MainTex_ST;
+    half _Cutoff;
+    half4 _Color;
+    half4 _Black;
+    // Dead Money
+    // Slot Count
+    half _SlotCount;
+    // Dead Money
 CBUFFER_END
 
 sampler2D _MainTex;
+// Dead Money
+// Support for slot color lookup table.
+sampler2D _SlotColorLUT;
+// Dead Money
 
 #endif // URP_LIT_INPUT_INCLUDED
--- spine-unity/Modules/com.esotericsoftware.spine.urp-shaders/Shaders/Include/Spine-Skeleton-ForwardPass-URP.hlsl
+++ spine-unity/Modules/com.esotericsoftware.spine.urp-shaders/Shaders/Include/Spine-Skeleton-ForwardPass-URP.hlsl
@@ -10,21 +10,29 @@ struct appdata {
 	float3 pos : POSITION;
 	half4 color : COLOR;
 	float2 uv0 : TEXCOORD0;
-#if defined(_TINT_BLACK_ON)
-	float2 tintBlackRG : TEXCOORD1;
-	float2 tintBlackB : TEXCOORD2;
-#endif
-
+	
+	// Dead Money
+	// Support for two masks and slot data.
+	float2 mask1uv : TEXCOORD1;
+	float2 mask2uv : TEXCOORD2;
+	float2 slotData : TEXCOORD3; // u: slot index, v: 0.0
+	// Dead Money
+	
 	UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct VertexOutput {
 	half4 color : COLOR0;
 	float2 uv0 : TEXCOORD0;
+	
+	// Dead Money
+	// Support for two masks and slot data.
+	float2 mask1uv : TEXCOORD1;
+	float2 mask2uv : TEXCOORD2;
+	float2 slotData : TEXCOORD3;
+	// Dead Money
+	
 	float4 pos : SV_POSITION;
-#if defined(_TINT_BLACK_ON)
-	float3 darkColor : TEXCOORD1;
-#endif
 	UNITY_VERTEX_OUTPUT_STEREO
 };
 
@@ -36,30 +44,63 @@ VertexOutput vert(appdata v) {
 	float3 positionWS = TransformObjectToWorld(v.pos);
 	o.pos = TransformWorldToHClip(positionWS);
 	o.uv0 = v.uv0;
+	o.mask1uv = v.mask1uv;
+	o.mask2uv = v.mask2uv;
+	o.slotData = v.slotData;
 	o.color = PMAGammaToTargetSpace(v.color);
-#if defined(_TINT_BLACK_ON)
-	o.color *= _Color;
-	o.darkColor = GammaToTargetSpace(
-		half3(v.tintBlackRG.r, v.tintBlackRG.g, v.tintBlackB.r)) + (_Black.rgb * v.color.a);
-#elif defined (APPLY_MATERIAL_TINT_COLOR)
+#if defined (APPLY_MATERIAL_TINT_COLOR)
 	o.color *= _Color;
 #endif
 	return o;
 }
 
-half4 frag(VertexOutput i) : SV_Target{
+// Dead Money
+// Mask Color Support
+inline float4 srgbToLinear(float4 c)
+{
+	// A simple gamma ≈2.2 approximation.
+	return float4(pow(c.rgb, 2.2), c.a);
+}
+
+inline float4 slotToLUTColor(float slotIndex, float colorIndex)
+{
+	float numSlots = _SlotCount;
+
+	float W = numSlots * 4.0;
+	float u = (slotIndex * 4 + colorIndex + 0.5) / (W);
+	float2 uv = float2(u, 0.0);
+	return tex2D(_SlotColorLUT, uv);
+}
+
+half4 frag(VertexOutput i) : SV_Target
+{
+	float slotIndex = i.slotData.x;
+
 	float4 texColor = tex2D(_MainTex, i.uv0);
-#if defined(_ZWRITE)
+	float4 black = float4(0.0, 0.0, 0.0, texColor.a);
+	float4 inkColor = float4(0.161, 0.114, 0.102, texColor.a);
+	inkColor = srgbToLinear(inkColor);
+
+	float4 mask1Color = tex2D(_MainTex, i.mask1uv);
+	float4 mask2Color = tex2D(_MainTex, i.mask2uv);
+
+	float4 lutColor1 = slotToLUTColor(slotIndex, 0);
+	float4 lutColor2 = slotToLUTColor(slotIndex, 1);
+
+	float4 colorA = lerp(black, lutColor1, mask1Color.r);
+	float4 colorB = lerp(colorA, lutColor2, mask1Color.b);
+
+	texColor = lerp(inkColor, colorB, mask1Color.r + mask1Color.b);
+
+	#if defined(_ZWRITE)
 	clip(texColor.a * i.color.a - _Cutoff);
-#endif
-#if defined(_TINT_BLACK_ON)
-	return fragTintedColor(texColor, i.darkColor, i.color, _Color.a, _Black.a);
-#else
+	#endif
+	
 	#if defined(_STRAIGHT_ALPHA_INPUT)
 	texColor.rgb *= texColor.a;
 	#endif
 	return (texColor * i.color);
-#endif
 }
+// Dead Money
 
 #endif
